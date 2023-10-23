6339118a9cf8ca8ee95e8bf51ed036e2a49e4c02
Muhammad Tayyab Tahir Qureshi <m.tayyabtq@gmail.com>
2023-10-23 09:59:10 +0500

    minor bug fixes

--- imm_gen.sv
+++ imm_gen.sv
@@ -19,13 +19,14 @@ module imm_gen
             
             7'b0100011: // S-type
                 imm_val = $signed({inst[31:25], inst[11:7]});
-            
             7'b1100011: // B-type
                 imm_val = $signed({inst[31], inst[7], inst[30:25], inst[11:8], 1'b0});
             7'b0110111: // U-type
                 imm_val = {inst[31:12],12'b0};
             7'b1101111: // J-type (JAL)
                 imm_val = $signed({inst[31], inst[19:12], inst[20], inst[30:21], 1'b0});
+            7'b1100111: // I-type (JALR)
+                imm_val = $signed(inst[31:20]);
         endcase
     end
 
